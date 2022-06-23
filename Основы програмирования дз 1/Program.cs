63b8a11c76b8643af39706a759a007133c5cfcd5
dacha358 <107683510+dacha358@users.noreply.github.com>
2022-06-23 10:44:51 +0300

    Добавьте файлы проекта.

---
+++ Основы програмирования дз 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Основы_програмирования_дз_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            char categoryOfRights;
+            string surname;
+            long landArea;
+            byte examGrade;
+            float health;
+            double bitcoinWallet;
+            bool isAngry;
+            sbyte eyeCondition;
+            uint bankValue;
+
+
+        }
+    }
+}
